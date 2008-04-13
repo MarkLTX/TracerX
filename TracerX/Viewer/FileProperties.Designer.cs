6a9c4ffb9580d16fa0943b365a7ee7c588f01f12
MarkLTX_cp <SND\MarkLTX_cp>
2008-04-13 03:00:32 +0000

    Added toolbar buttons for expand all, file properties, open file, and toggling relative vs. absolute timestamps.

--- TracerX/Viewer/FileProperties.Designer.cs
+++ TracerX/Viewer/FileProperties.Designer.cs
@@ -24,25 +24,40 @@ namespace TracerX.Viewer {
         /// </summary>
         private void InitializeComponent() {
             this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.close = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // propertyGrid1
             // 
             this.propertyGrid1.CommandsDisabledLinkColor = System.Drawing.Color.Black;
             this.propertyGrid1.CommandsVisibleIfAvailable = false;
-            this.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Top;
             this.propertyGrid1.HelpVisible = false;
             this.propertyGrid1.Location = new System.Drawing.Point(0, 0);
             this.propertyGrid1.Name = "propertyGrid1";
-            this.propertyGrid1.Size = new System.Drawing.Size(343, 398);
+            this.propertyGrid1.Size = new System.Drawing.Size(350, 269);
             this.propertyGrid1.TabIndex = 4;
+            // 
+            // close
+            // 
+            this.close.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.close.Location = new System.Drawing.Point(263, 275);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(75, 23);
+            this.close.TabIndex = 5;
+            this.close.Text = "Close";
+            this.close.UseVisualStyleBackColor = true;
             // 
             // FileProperties
             // 
+            this.AcceptButton = this.close;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(343, 398);
+            this.CancelButton = this.close;
+            this.ClientSize = new System.Drawing.Size(350, 302);
+            this.Controls.Add(this.close);
             this.Controls.Add(this.propertyGrid1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "FileProperties";
             this.ShowInTaskbar = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
@@ -54,6 +69,7 @@ namespace TracerX.Viewer {
         #endregion
 
         private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.Button close;
 
     }
 }
