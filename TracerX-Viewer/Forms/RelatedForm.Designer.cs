4914aeccecb6f1b07bfddc1f81fb58ce89c74fe0
MarkLTX_cp <SND\MarkLTX_cp>
2017-11-02 23:34:17 +0000

    Renamed "related files" to "special files"

--- TracerX-Viewer/Forms/RelatedForm.Designer.cs
+++ TracerX-Viewer/Forms/RelatedForm.Designer.cs
@@ -38,12 +38,13 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // linkProgramData
             // 
             this.linkProgramData.AutoSize = true;
-            this.linkProgramData.Location = new System.Drawing.Point(12, 36);
+            this.linkProgramData.Location = new System.Drawing.Point(12, 56);
             this.linkProgramData.Name = "linkProgramData";
             this.linkProgramData.Size = new System.Drawing.Size(69, 13);
             this.linkProgramData.TabIndex = 0;
@@ -54,7 +55,7 @@
             // linkRecentFolders
             // 
             this.linkRecentFolders.AutoSize = true;
-            this.linkRecentFolders.Location = new System.Drawing.Point(24, 68);
+            this.linkRecentFolders.Location = new System.Drawing.Point(24, 88);
             this.linkRecentFolders.Name = "linkRecentFolders";
             this.linkRecentFolders.Size = new System.Drawing.Size(90, 13);
             this.linkRecentFolders.TabIndex = 1;
@@ -65,7 +66,7 @@
             // linkRecentFiles
             // 
             this.linkRecentFiles.AutoSize = true;
-            this.linkRecentFiles.Location = new System.Drawing.Point(24, 52);
+            this.linkRecentFiles.Location = new System.Drawing.Point(24, 72);
             this.linkRecentFiles.Name = "linkRecentFiles";
             this.linkRecentFiles.Size = new System.Drawing.Size(100, 13);
             this.linkRecentFiles.TabIndex = 2;
@@ -76,7 +77,7 @@
             // linkLocalAppData
             // 
             this.linkLocalAppData.AutoSize = true;
-            this.linkLocalAppData.Location = new System.Drawing.Point(12, 104);
+            this.linkLocalAppData.Location = new System.Drawing.Point(12, 124);
             this.linkLocalAppData.Name = "linkLocalAppData";
             this.linkLocalAppData.Size = new System.Drawing.Size(75, 13);
             this.linkLocalAppData.TabIndex = 3;
@@ -87,7 +88,7 @@
             // linkEvents
             // 
             this.linkEvents.AutoSize = true;
-            this.linkEvents.Location = new System.Drawing.Point(24, 120);
+            this.linkEvents.Location = new System.Drawing.Point(24, 140);
             this.linkEvents.Name = "linkEvents";
             this.linkEvents.Size = new System.Drawing.Size(92, 13);
             this.linkEvents.TabIndex = 4;
@@ -98,7 +99,7 @@
             // lblAllUsers
             // 
             this.lblAllUsers.AutoSize = true;
-            this.lblAllUsers.Location = new System.Drawing.Point(87, 36);
+            this.lblAllUsers.Location = new System.Drawing.Point(87, 56);
             this.lblAllUsers.Name = "lblAllUsers";
             this.lblAllUsers.Size = new System.Drawing.Size(136, 13);
             this.lblAllUsers.TabIndex = 5;
@@ -107,7 +108,7 @@
             // lblCurUser
             // 
             this.lblCurUser.AutoSize = true;
-            this.lblCurUser.Location = new System.Drawing.Point(87, 104);
+            this.lblCurUser.Location = new System.Drawing.Point(87, 124);
             this.lblCurUser.Name = "lblCurUser";
             this.lblCurUser.Size = new System.Drawing.Size(71, 13);
             this.lblCurUser.TabIndex = 6;
@@ -116,7 +117,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(130, 52);
+            this.label2.Location = new System.Drawing.Point(130, 72);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(143, 13);
             this.label2.TabIndex = 7;
@@ -125,7 +126,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(120, 68);
+            this.label3.Location = new System.Drawing.Point(120, 88);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(218, 13);
             this.label3.TabIndex = 8;
@@ -134,17 +135,27 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(122, 120);
+            this.label4.Location = new System.Drawing.Point(122, 140);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(183, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "(event log msgs are also logged here)";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "These are the \"special\" files and folders used by TracerX.";
+            // 
             // RelatedForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(370, 261);
+            this.ClientSize = new System.Drawing.Size(370, 192);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -157,7 +168,7 @@
             this.Controls.Add(this.linkProgramData);
             this.Name = "RelatedForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Related Files & Folders";
+            this.Text = "Special Files & Folders";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,5 +186,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
     }
 }
