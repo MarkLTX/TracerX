6bdf07926f18d841f0ef1957f7c6689688cd1e22
Mark Lauritsen <malauritsen@eprod.com>
2018-07-09 11:12:38 -0500

    Viewer: Honor user settings regarding how often to check for a new release.

--- TracerX-Viewer/VersionChecker.cs
+++ TracerX-Viewer/VersionChecker.cs
@@ -12,20 +12,27 @@ using System.IO;
 
 namespace TracerX
 {
-    // This class looks for a newer version of TracerX on GitHub using a
+    // This class looks for a newer version of the TracerX binaries on GitHub using a
     // worker thread to get the HTML for the TracerX releases page.
     // It would be better to use the GitHub API but that would require
     // adding a dependency to a NuGet package like Octokit, I think.
     internal static class VersionChecker
     {
+        /// <summary>
+        /// Event raised on the GUI thread if the worker thread started by CheckForNewVersion() finds a new version.
+        /// </summary>
         public static event EventHandler NewVersionFound;
 
         const string _url = "http://github.com/MarkLTX/TracerX/releases";
 
+        /// <summary>
+        /// Starts a worker thread that checks if a new version of the TracerX binaries is available on GitHub.
+        /// May lead to NewVersionFound being raised on the GUI thread.
+        /// </summary>
         public static void CheckForNewVersion()
         {
-            //if (Settings.Default.VersionCheckingAllowed &&
-            //    Settings.Default.VersionLastChecked.Date.AddDays(Settings.Default.VersionCheckInterval) < DateTime.Now) 
+            if (Settings.Default.VersionCheckingAllowed &&
+                Settings.Default.VersionLastChecked.Date.AddDays(Settings.Default.VersionCheckInterval) < DateTime.Now) 
             {
                 // Download the TracerX releases webpage in a worker thread.
                 BackgroundWorker bw = new BackgroundWorker();
@@ -40,21 +47,23 @@ namespace TracerX
             try
             {
                 // Download the TracerX releases webpage from github.com.
-                // Find the strings that look like release numbers an compare the
+                // Find the strings that look like release numbers and compare the
                 // "largest" release found to the version number of the current executable. 
                 // Each release should have a link like this.
                 // <a href="/MarkLTX/TracerX/releases/tag/v7.1">TracerX 7.1</a>
 
-                // These two statements prevent an exception with error message: The request was aborted: Could not create SSL/TLS secure channel.
+                // These two statements prevent an exception with this error message: "The request was aborted: Could not create SSL/TLS secure channel."
                 ServicePointManager.Expect100Continue = true;
-                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12; // Added to .NET Framework 4.5
 
                 WebClient client = new WebClient();
                 string webPage = client.DownloadString(_url);
                 string key = "/MarkLTX/TracerX/releases/tag/v";
                 int pos = webPage.IndexOf(key);
 
-                while (pos != -1)
+                // Assume the latest release is listed first, which is the only one needed.
+
+                if (pos != -1)
                 {
                     // Get the next substring of characters that are numbers and periods and attempt to parse it into a Version.
 
@@ -80,43 +89,17 @@ namespace TracerX
                         if (newestVer > Assembly.GetExecutingAssembly().GetName().Version)
                         {
                             e.Result = true;
-                            break;
+                            //break;
                         }
                     }
 
-                    pos = webPage.IndexOf(key, pos);
+                    //pos = webPage.IndexOf(key, pos);
                 }
             }
             catch (Exception ex)
             {
                 Debug.Print("{0}", ex);
             }
-        }
-
-        private static string DownloadUrl()
-        {
-            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(_url);
-            req.Method = "GET";
-
-            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-
-            // Skip validation of SSL/TLS certificate
-            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
-
-            WebResponse respon = req.GetResponse();
-            Stream res = respon.GetResponseStream();
-            string ret = "";
-            byte[] buffer = new byte[1048];
-            int read = 0;
-
-            while ((read = res.Read(buffer, 0, buffer.Length)) > 0)
-            {
-                //Console.Write(Encoding.ASCII.GetString(buffer, 0, read));
-                ret += Encoding.ASCII.GetString(buffer, 0, read);
-            }
-
-            return ret;
         }
 
         static void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
